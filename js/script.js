d89007639951f492f824ad1aa2e9392e5b5bd085
NexSham Technologies <krishimitraai25@email.com>
2026-07-08 12:48:44 +0530

    website

--- js/script.js
+++ js/script.js
@@ -1,3 +1,47 @@
+'use strict';
+
+/**
+ * navbar variables
+ */
+const menuToggleBtn = document.querySelector("[data-navbar-toggle-btn]");
+const navbar = document.querySelector("[data-navbar]");
+
+/**
+ * element toggle function
+ */
+
+const elemToggleFunc = function (elem) { elem.classList.toggle("active"); }
+
+menuToggleBtn.addEventListener("click", function () { elemToggleFunc(navbar); });
+
+
+
+
+/**
+ * go to top
+ */
+
+const goTopBtn = document.querySelector("[data-go-top]");
+
+window.addEventListener("scroll", function () {
+
+  if (window.scrollY >= 800) {
+    goTopBtn.classList.add("active");
+  } else {
+    goTopBtn.classList.remove("active");
+  }
+
+});
+
+// _____________________________________________
+
+
+
+
+
+
+
+
 /* ==========================================================
    NEXSHAM HERO
 ========================================================== */
@@ -135,3 +179,16 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
